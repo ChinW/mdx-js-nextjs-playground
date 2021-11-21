224c8974dc3186f556504a148f1d57ebcb2c9571
chi <contractswif@hotmail.com>
2021-11-21 19:39:53 +0800

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MDXProvider } from "@mdx-js/react";
+
+const H1 = (...props: any) => {
+  return <h1>hello 3</h1>;
+};
+
+const components = {
+  h2: H1,
+  h3: H1,
+  h4: H1,
+  h5: H1,
+};
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  console.log("Component", Component);
+  return (
+    <MDXProvider components={components}>
+      <Component />
+    </MDXProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
